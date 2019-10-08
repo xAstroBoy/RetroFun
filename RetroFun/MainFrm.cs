2725a46b7c9592d814eb8822cc10db7c419d84d4
Marco Corriero <16446639+marcocorriero@users.noreply.github.com>
2019-10-08 18:44:27 +0200

    Ops Forgot to bind the RoomUserWalk xD

--- RetroFun/MainFrm.cs
+++ RetroFun/MainFrm.cs
@@ -60,7 +60,7 @@ namespace RetroFun
         }
 
 
-
+        [OutDataCapture("RoomUserWalk")]
         public void OnOutUserWalk(DataInterceptedEventArgs e)
         {
             if (FreezeUserMovement)
